7161b925ccbe152c7b660795ff86af99eab542cd
IURII <pikuliurii@gmail.com>
2022-07-13 14:14:29 -0300

    add project 2 React

---
+++ src/components/countersList.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+    const initialState = [
+        {id:0, value: 0, name: 'Ненужная вещь'},
+        {id:1, value: 4, name: 'Ложка'},
+        {id:2, value: 0, name: 'Вилка'},
+        {id:3, value: 0, name: 'Тарелка'},
+        {id:4, value: 0, name: 'Набор минималиста'}
+    ];
+
+    const [counters, setCounters] = useState(initialState);
+
+    const handleDelete = (id) => {
+        const newCounters = counters.filter(c => c.id !== id);
+        setCounters(newCounters);
+    }
+
+    const handleReset = () => {
+        setCounters(initialState);
+    }
+
+    const handleIncrement = (id) => {
+        setCounters(counters.map(counter => {
+            if (counter.id === id) {
+                counter.value += 1;
+            }
+            return counter;
+        })
+        )
+     }
+ 
+     const handleDecrement = (id) => {
+        setCounters(counters.map(counter => {
+            if (counter.id === id) {
+                counter.value -= 1;
+            }
+
+            return counter;
+            })
+        )
+     }
+        
+    return ( 
+    <>
+        {counters.map(count => (
+            <Counter 
+                key={count.id} 
+                {...count}
+                onDelete={handleDelete}
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+            />
+        ))}
+        <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Сброс</button>   
+    </>
+    );
+}
+
+export default CountersList;
